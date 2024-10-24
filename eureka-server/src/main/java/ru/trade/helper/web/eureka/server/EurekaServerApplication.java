59a5b7c2c28513f4c1c3b7681248085db454d224
dm <d791232344656@yandex.ru>
2024-10-24 12:53:51 +0300

    eureka server settings done

---
+++ eureka-server/src/main/java/ru/trade/helper/web/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package ru.trade.helper.web.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
