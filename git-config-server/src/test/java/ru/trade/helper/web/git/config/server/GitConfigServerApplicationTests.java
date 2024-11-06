687394e088fb478b36fb4ec9bee6817cb41a2b1b
dm <d791232344656@yandex.ru>
2024-11-06 15:57:45 +0300

    add git config server

---
+++ git-config-server/src/test/java/ru/trade/helper/web/git/config/server/GitConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.trade.helper.web.git.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+@EnableConfigServer
+class GitConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
